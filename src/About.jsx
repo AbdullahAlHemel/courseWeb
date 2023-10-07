a2281cafaefb0e8cb4c791122003b5c1254407db
AbdullahAlHemel <abdullahalhemel100@gmail.com>
2023-10-08 03:06:09 +0600

    NavBar added

--- src/About.jsx
+++ src/About.jsx
@@ -6,7 +6,7 @@ import { useEffect } from 'react';
 
 const About = () => {
     useEffect(() => {
-        AOS.init({duration:"1000"})
+        AOS.init({duration:"1100"})
        })
     
     return (
