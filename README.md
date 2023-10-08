e42ecb60aa7a35b1124560453f235da612efe4cf
AbdullahAlHemel <abdullahalhemel100@gmail.com>
2023-10-08 11:37:22 +0600

    Feature added

--- README.md
+++ README.md
@@ -1,7 +1,34 @@
 In this website you can you develop your skill and grow up your needs.here the five points:
 
 1. Six important courses help to develop our everyday lifeStyle and survival power, this courses shows in box with six different box.
-2. Limited price set because anybody can buy this courses and develop this skill according to his n eeds.
+
+2. Limited price set because anybody can buy this courses and develop this skill according to his needs.
+
 3. Every card box has different route where details information is available, then image, price and name also added.
+
 4. Daily Life Hacks are available in those courses, which is need for everybody in every where.
+
 5. Make colorful, animated and shortcut which save time and show information, anybody can visit to know those information from every place.
+
+
+
+
+
+
+Answer of 5 MCQ:
+
+1. A: {} 
+Reason: we make a variable let and which was void , for this after console get no null array When we use bracket notation, without anything normally keep void .
+
+2. C: "12"
+Reason: create a function and take 2 parameters a and b ,then return those . after call back ,take  value of parameters first number 1 and a string '2', then return one number and a string.
+
+3. A: ['🍕', '🍫', '🥑', '🍔']
+Reason: created a array with 4 favorite food, then declare a array to add extra food but console array which first declared ,as a result extra food will not added in array. 
+
+4. B: Hi there, undefined
+Reason: create a function and keep one parameters and return it but when it called there was no value for this text show but another part isn't valid, Cannot read property "size" of undefined.
+
+5. C: 3
+Reason: create variables first one number then an array , loop it and add condition that and add number 0-3 ,there are 3 number exists
+
--- src/pages/Home/News.jsx
+++ src/pages/Home/News.jsx
@@ -17,10 +17,10 @@ const News = () => {
         
             <div className='grid md:grid-cols-1'>
                 
-                <h2>{id}</h2>
-                <div className='w-[900px] mx-auto'>
-                      <div className='w-[700px] mx-auto'><img  src={coin} alt="" /></div>
-                   <h2 className='mt-8 text-3xl font-semibold my-4 text-center'>Financial freedom</h2>
+                {/* <h2>{id}</h2> */}
+                <div className='w-[900px] mx-auto mt-8'>
+                      <div className='w-[700px] mx-auto rounded'><img  src={coin} alt="" /></div>
+                   <h2 className='mt-8 text-3xl font-semibold my-4 text-center'>Financial freedom <span className='text-[15px] ml-6 text-gray-500'>at</span><span className='ml-2 text-gray-500 font-medium'>250 $</span></h2>
                    <p className='font-medium pb-3 leading-8 text-justify'>Attaining financial freedom is an objective for most individuals. Financial freedom usually means having enough savings, financial investments, and cash on hand to afford the kind of life we desire for ourselves and our families. It means growing savings that enable us to retire or pursue the career we want without being driven by earning a set salary each year. Financial freedom means our money is working for us rather than the other way around.How do you become financially free?To become financially free, you must pay off your consumer debts, build a safety net of savings funds, and create enough passive income through investing or business ownership to pay for your current and expected future living expenses. We are burdened with increasing debt, monetary emergencies, excessive consumer spending, and other problems that keep us from reaching our most meaningful financial objectives. Such challenges confront everybody, but the following twelve habits can put you on the ideal path to financial wellness.</p>
                     <h2 className='mb-24 text-blue-600'>#money  #economy  #dollor</h2>
                 </div>
